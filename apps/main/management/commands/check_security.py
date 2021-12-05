3c70b0acb0ca268d1136219847a605874e8d1d98
gotlium <gotlium@gmail.com>
2021-12-05 15:38:36 +0500

    add security check command

--- apps/main/management/commands/check_security.py
+++ apps/main/management/commands/check_security.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand
-from main.models import Site
+from main.models import Site, SiteVulnerabilities
+
+from .create_sites import execute
+from json import loads
 
 
 class Command(BaseCommand):
@@ -9,4 +12,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for site in Site.objects.all():
-            pass
+            if SiteVulnerabilities.objects.filter(site=site).count() > 0:
+                continue
+            data = []
+            if site.php_version:
+                res = execute(f'searchsploit -j -c PHP {site.php_version}')
+                data = loads(''.join(res[1]))["RESULTS_EXPLOIT"]
+            if site.framework_name:
+                res = execute(f'searchsploit -j -c {site.framework_name}')
+                data.extend(loads(''.join(res[1]))["RESULTS_EXPLOIT"])
+            if site.os_version:
+                res = execute(f'searchsploit -j -c {site.os_version}')
+                data.extend(loads(''.join(res[1]))["RESULTS_EXPLOIT"])
+            for row in data:
+                SiteVulnerabilities.objects.create(
+                    site=site,
+                    text=row['Title'],
+                )
